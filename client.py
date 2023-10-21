e15866a0eacea9c01f9901b66228951ce66eb121
Arthur Padilha <99270875+P4d1lh4@users.noreply.github.com>
2023-10-21 18:46:36 -0300

    Update client.py with recognition plus negative recognition

--- client.py
+++ client.py
@@ -5,18 +5,18 @@ def checksum(data):
     print(f"N. bytes: {byte_count}")
     return
 
+def ping(host, port):
+    client = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    addr = (host, port)
+    data = b"ping"
+    client.sendto(data, addr)
+    data, addr = client.recvfrom(1024)
+    if data == b"pong":
+        print(f"{host} is up!")
+    else:
+        print(f"{host} is down!")
 
 if __name__ == "__main__":
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
-    client = s.socket(s.AF_INET, s.SOCK_DGRAM)
-    addr = (HOST, PORT)
-
-    while True:
-        data = input("Enter a word: ")
-        data = data.encode("utf-8")
-        checksum(data)
-        client.sendto(data, addr)
-        data, addr = client.recvfrom(1024)
-        data = data.decode("utf-8")
-        print(len(data))
+    ping(HOST, PORT)
