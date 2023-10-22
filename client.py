9edd53eed4f7b0f9e9cf447fc6fc6e3025727664
Luis Adolfo <luisadolfoalves@gmail.com>
2023-10-22 16:51:18 -0300

    option to send either isolated packet or batch format

--- client.py
+++ client.py
@@ -29,12 +29,35 @@ if __name__ == "__main__":
     while True:
         data = input("Enter a word: ")
         data = data.encode("utf-8")
-        checksum_value = checksum(data)
-        checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
-        client.sendto(checksum_bytes, addr)
-        client.sendto(data, addr)
+
+        send_mode = input("Send mode (isolated/batch): ")
+
+        if send_mode == "isolated":
+            checksum_value = checksum(data)
+            checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
+            client.sendto(checksum_bytes, addr)
+            client.sendto(data, addr)
+        elif send_mode == "batch":
+            packets = []
+
+            while True:
+                data = input("Enter a word (or empty line to finish): ")
+                data = data.encode("utf-8")
+
+                if not data:
+                    break
+
+                checksum_value = checksum(data)
+                checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
+                packets.append((checksum_bytes, data))
+
+            for checksum_bytes, data in packets:
+                client.sendto(checksum_bytes, addr)
+                client.sendto(data, addr)
+
         data, addr = client.recvfrom(1024)
         data = data.decode("utf-8")
+
         if data == "ERRO":
             print("Falha de integridade")
         else:
