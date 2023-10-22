bed81de1ba971c9779409f7bff47347c31b42405
FernandoM <89084956+Nand0000000000@users.noreply.github.com>
2023-10-22 17:07:50 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 import socket as s
+import time
 
 def checksum(data):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
+    
+def handle_packet(data, seq_num, addr, server_socket, error_probability):
+    # Verifique a integridade do pacote (exemplo simples)
+    if b'X' in data:  # Simula erro de corrupção
+        print("Received a corrupted packet, ignoring.")
+        return
 
 if __name__ == "__main__":
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
     server = s.socket(s.AF_INET, s.SOCK_DGRAM)
     server.bind((HOST, PORT))
+    server.settimeout(5)  # Tempo limite para receber reconhecimentos
     expected_seq_num = 0
 
     while True:
@@ -22,7 +30,8 @@ if __name__ == "__main__":
             data = data.upper()
             data = data.encode("utf-8")
             checksum(data)
-            server.sendto(data, addr)
+            server.sendto(str(seq_num).encode(), addr)  # Envia o reconhecimento
             expected_seq_num += 1
         else:
-            print(f"Received out-of-order packet with seq_num {seq_num}. Ignoring.")
+            print(f"Received out-of-order packet with seq_num {seq_num}. Sending NACK.")
+            server.sendto(str(expected_seq_num - 1).encode(), addr)  # Envia NACK
