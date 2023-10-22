0d0a4892e3c46e210f41ec40bf342749a610ebcc
Luis Adolfo <luisadolfoalves@gmail.com>
2023-10-22 16:12:04 -0300

    checksum done

--- client.py
+++ client.py
@@ -3,7 +3,21 @@ import socket as s
 def checksum(data):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
-    return
+    
+    if byte_count % 2 == 1:
+        data += b'\x00'
+        byte_count += 1
+
+    checksum_value = 0
+
+    for i in range(0, byte_count, 2):
+        w = (data[i] << 8) + data[i + 1]
+        checksum_value += w
+        checksum_value = (checksum_value >> 16) + (checksum_value & 0xFFFF)
+
+    checksum_value = ~checksum_value & 0xFFFF
+
+    return checksum_value
 
 
 if __name__ == "__main__":
@@ -15,7 +29,9 @@ if __name__ == "__main__":
     while True:
         data = input("Enter a word: ")
         data = data.encode("utf-8")
-        checksum(data)
+        checksum_value = checksum(data)
+        checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
+        client.sendto(checksum_bytes, addr)
         client.sendto(data, addr)
         data, addr = client.recvfrom(1024)
         data = data.decode("utf-8")
--- server.py
+++ server.py
@@ -1,9 +1,24 @@
 import socket as s
 
-def checksum(data):
+def verify_checksum(data, checksum):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
-    return
+    
+    if byte_count % 2 == 1:
+        data += b'\x00'
+        byte_count += 1
+
+    checksum_value = 0
+
+    for i in range(0, byte_count, 2):
+        w = (data[i] << 8) + data[i + 1]
+        checksum_value += w
+        checksum_value = (checksum_value >> 16) + (checksum_value & 0xFFFF)
+
+    checksum_value = ~checksum_value & 0xFFFF
+
+    return checksum_value == checksum
+
 
 if __name__ == "__main__":
     
@@ -13,11 +28,17 @@ if __name__ == "__main__":
     server.bind((HOST, PORT))
 
     while True:
+        checksum_bytes, addr = server.recvfrom(1024)
+        checksum = int.from_bytes(checksum_bytes, byteorder="big")
         data, addr = server.recvfrom(1024)
         data = data.decode("utf-8")
 
         print("Client: {data}")
+        if verify_checksum(data.encode("utf-8"), checksum):
+            print("Checksum is valid.")
+        else:
+            print("Checksum is invalid.")
+
         data = data.upper()
         data = data.encode("utf-8")
-        checksum(data)
         server.sendto(data, addr)
