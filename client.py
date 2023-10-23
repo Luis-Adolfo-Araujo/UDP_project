8e8f49d7b7da69abe2859d9c815cfb0573f749a7
FernandoM <89084956+Nand0000000000@users.noreply.github.com>
2023-10-22 21:26:25 -0300

    Update client.py

--- client.py
+++ client.py
@@ -19,46 +19,37 @@ def checksum(data):
 
     return checksum_value
 
+def ping(host, port):
+    client = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    addr = (host, port)
+    data = b"ping"
+    client.sendto(data, addr)
+    data, addr = client.recvfrom(1024)
+    if data == b"ping":
+        print(f"{host} is up!")
+    else:
+        print(f"{host} is down!")
+
 
 if __name__ == "__main__":
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
     client = s.socket(s.AF_INET, s.SOCK_DGRAM)
     addr = (HOST, PORT)
+    
+    client.settimeout(1)
 
     while True:
+        ping(HOST, PORT)
         data = input("Enter a word: ")
         data = data.encode("utf-8")
-
-        send_mode = input("Send mode (isolated/batch): ")
-
-        if send_mode == "isolated":
-            checksum_value = checksum(data)
-            checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
-            client.sendto(checksum_bytes, addr)
-            client.sendto(data, addr)
-        elif send_mode == "batch":
-            packets = []
-
-            while True:
-                data = input("Enter a word (or empty line to finish): ")
-                data = data.encode("utf-8")
-
-                if not data:
-                    break
-
-                checksum_value = checksum(data)
-                checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
-                packets.append((checksum_bytes, data))
-
-            for checksum_bytes, data in packets:
-                client.sendto(checksum_bytes, addr)
-                client.sendto(data, addr)
-
+        checksum_value = checksum(data)
+        checksum_bytes = checksum_value.to_bytes(2, byteorder="big")
+        client.sendto(checksum_bytes, addr)
+        client.sendto(data, addr)
         data, addr = client.recvfrom(1024)
         data = data.decode("utf-8")
-
         if data == "ERRO":
             print("Falha de integridade")
         else:
-            print(len(data))
+            print("pacote sem falha de integridade")
